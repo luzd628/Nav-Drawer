570ecee681b9baa9d803a05a41d6f756a87adb35
luzd <dzulfikri628@gmail.com>
2023-05-23 23:04:32 +0700

    Second Commit

--- app/src/main/java/com/dzul/mynavdrawer/MainActivity.kt
+++ app/src/main/java/com/dzul/mynavdrawer/MainActivity.kt
@@ -3,18 +3,27 @@ package com.dzul.mynavdrawer
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.dzul.mynavdrawer.ui.theme.MyNavDrawerTheme
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -35,14 +44,41 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MyNavDrawerApp(){
+
+    val scaffoldState = rememberScaffoldState()
+    val scope = rememberCoroutineScope()
+    val context = LocalContext.current
+
     Scaffold(
+        scaffoldState = scaffoldState,
         topBar = {
             MyTopBar (
                 onMenuClick = {
+                    scope.launch {
+                        scaffoldState.drawerState.open()
 
+                    }
                 }
             )
-        }
+        },
+
+        drawerContent = {
+            //Text(stringResource(R.string.hello_from_nav_drawer))
+            MyDrawerContent(
+                onItemSelected = {title ->
+                    scope.launch {
+                        scaffoldState.drawerState.close()
+                        scaffoldState.snackbarHostState.showSnackbar(
+                            message = context.resources.getString(R.string.coming_soon,title),
+                            actionLabel = context.resources.getString(R.string.subscribe_question)
+                        )
+                    }
+                }
+            )
+        },
+
+        drawerGesturesEnabled = scaffoldState.drawerState.isOpen,
+
     ) { paddingValues ->
         Box(
             modifier = Modifier
@@ -52,6 +88,57 @@ fun MyNavDrawerApp(){
         ){
             Text(stringResource(R.string.hello_world))
         }
+    }
+}
+
+data class MenuItem(val title :String,val icon:ImageVector )
+
+@Composable
+fun MyDrawerContent(
+    modifier: Modifier = Modifier,
+    onItemSelected: (title : String) ->Unit,
+){
+    val items = listOf(
+        MenuItem(
+            title = stringResource(R.string.home),
+            icon = Icons.Default.Home
+        ),
+        MenuItem(
+            title = stringResource(R.string.favourite),
+            icon = Icons.Default.Favorite
+        ),
+        MenuItem(
+            title = stringResource(R.string.profile),
+            icon = Icons.Default.AccountCircle
+        ),
+    )
+
+    Column(modifier.fillMaxSize()) {
+        Box (
+            modifier = Modifier
+                .height(190.dp)
+                .fillMaxWidth()
+                .background(MaterialTheme.colors.primary)
+        )
+
+        for (item in items){
+            Row(
+                modifier = Modifier
+                    .clickable { onItemSelected(item.title) }
+                    .padding(vertical = 12.dp, horizontal = 16.dp)
+                    .fillMaxWidth(),
+
+            ) {
+                Icon(
+                    imageVector = item.icon,
+                    contentDescription = item.title,
+                    tint = Color.DarkGray
+                )
+                Spacer(modifier = Modifier.width(32.dp))
+                Text(text = item.title, style = MaterialTheme.typography.subtitle2)
+            }
+        }
+        Divider()
     }
 }
 
